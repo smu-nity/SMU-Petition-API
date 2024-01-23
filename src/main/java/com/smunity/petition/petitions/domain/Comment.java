e5bd8779e87aa9e3a8c1da8c1561833cb0ed075d
600gramSik <geunsikevin@naver.com>
2024-01-23 15:24:17 +0900

    ✨ feature: Petition, Respond, Agreement, Comment 엔티티 설계(#3

---
+++ src/main/java/com/smunity/petition/petitions/domain/Comment.java
@@ -0,0 +1,27 @@
+package com.smunity.petition.petitions.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment {
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id")
+    private User user;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "petiion_id")
+    private Petition petition;
+
+    private String content;
+
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+}
