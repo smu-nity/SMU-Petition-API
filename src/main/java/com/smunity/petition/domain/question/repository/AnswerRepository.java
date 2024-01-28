d61aa1b5216c368a0b5f889b72cc8350e179073d
hyunmin0317 <choihm9903@naver.com>
2024-01-28 21:35:00 +0900

    ✨ feat: petiton 도메인 리포지토리 작성 (#4)

---
+++ src/main/java/com/smunity/petition/domain/question/repository/AnswerRepository.java
@@ -0,0 +1,7 @@
+package com.smunity.petition.domain.question.repository;
+
+import com.smunity.petition.domain.question.entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Long> {
+}
