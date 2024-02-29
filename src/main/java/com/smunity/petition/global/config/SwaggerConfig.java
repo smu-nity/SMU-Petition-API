f1cdc52437dacff013cc423d868cd240a02d65df
jiiiiiw <kijiwi1@naver.com>
2024-02-29 16:16:44 +0900

    :sparkles: feat: SwaggerConfig 부분 추가 작성 (#27)

--- src/main/java/com/smunity/petition/global/config/SwaggerConfig.java
+++ src/main/java/com/smunity/petition/global/config/SwaggerConfig.java
@@ -1,8 +1,11 @@
 package com.smunity.petition.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,7 +13,10 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI getOpenApi() {
-        return new OpenAPI().info(getSwaggerInfo());
+
+        return new OpenAPI().info(getSwaggerInfo())
+                .components(authSetting())
+                .addSecurityItem(new SecurityRequirement().addList("access-token"));
     }
 
     private Info getSwaggerInfo() {
@@ -22,5 +28,15 @@ public class SwaggerConfig {
                 .description("Smunity의 API 문서 입니다.")
                 .version("v0.0.1")
                 .license(license);
+    }
+
+    private Components authSetting() {
+        return new Components()
+                .addSecuritySchemes(
+                        "access-token",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT"));
     }
 }
